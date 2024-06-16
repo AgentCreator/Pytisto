a069fb6ad78723961bdc406c5b45722b79a4d074
AgentCreator <320320ua@gmail.com>
2024-06-16 07:39:25 +0300

    Version 1.4.1
    new naming scheme, changed the setup.py to say the accurate version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(
     name="Pytisto",
-    version="1.0",
-    packages=["pytisto", "pytisto.expr_tests"],
+    version="1.4.1",
+    packages=["pytisto", "pytisto.expr_tests", "pytisto.main"],
     license="MIT",
     description="a fast, simple, powerful and free test framework for Python.",
     url="https://github.com/AgentCreator/Pytisto"
